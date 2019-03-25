c16a8e078036a55c0b959c070e09351f60223069
Rafael Melo <rsmelo_@hotmail.com>
2019-03-25 18:52:09 -0300

    Havok

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
 class GraphNode {
     constructor({ identifier, attributes }) {
+        this.removeAttribute = (attr) => {
+            if (attr in this.attributes) {
+                delete this.attributes[attr];
+            }
+            else {
+                console.log("Attribute not found");
+            }
+        };
         this.identifier = identifier;
         this.attributes = attributes;
     }
 }
 const tourists = [
-    new GraphNode({ identifier: 'dart', attributes: { lang: 'en' } }),
-    new GraphNode({ identifier: 'ismelo', attributes: { lang: 'en' } }),
-    new GraphNode({ identifier: 'fgod', attributes: {} }),
-    new GraphNode({ identifier: 'murtinha', attributes: {} })
+    new GraphNode({
+        identifier: "dart",
+        attributes: { lang: ["en", "fr", "pt", "es"], age: 21 }
+    }),
+    new GraphNode({
+        identifier: "ismelo",
+        attributes: { lang: ["en", "fr", "al"], age: 12 }
+    })
+    // new GraphNode({
+    //   identifier: "fgod",
+    //   attributes: { lang: ["pt", "fr", "al"] }
+    // }),
+    // new GraphNode({
+    //   identifier: "murtinha",
+    //   attributes: { lang: ["es", "fr", "al"] }
+    // })
 ];
 class Grafo {
     constructor(nodes) {
         this.links = [];
         this.createLinks = () => {
+            this.links = [];
             this.nodes.forEach((node) => {
                 Object.keys(node.attributes).forEach((attr) => {
-                    const value = node.attributes[attr];
+                    const values = Array.isArray(node.attributes[attr])
+                        ? node.attributes[attr]
+                        : [node.attributes[attr]];
                     this.nodes.forEach((otherNode) => {
                         if (node === otherNode)
                             return;
-                        if (attr in node.attributes && otherNode.attributes[attr] === value) {
-                            this.links.push({
-                                identifier: attr,
-                                connections: [node, otherNode]
+                        if (attr in node.attributes) {
+                            const newArray = Array.isArray(otherNode.attributes[attr])
+                                ? otherNode.attributes[attr]
+                                : [otherNode.attributes[attr]];
+                            values.forEach((val) => {
+                                if (newArray.includes(val) || val in newArray) {
+                                    this.links.push({
+                                        identifier: attr,
+                                        connections: [node, otherNode]
+                                    });
+                                }
                             });
                         }
                     });
@@ -32,8 +62,32 @@ class Grafo {
             console.table(this.nodes);
             console.table(this.links);
         };
+        this.addNode = (node) => {
+            this.nodes.push(node);
+            this.createLinks();
+        };
+        this.removeNodeByIdentifier = (identifier) => {
+            const newNodes = this.nodes.filter(node => node.identifier != identifier);
+            if (newNodes.length == this.nodes.length) {
+                console.log("Node not found");
+            }
+            else {
+                this.nodes = newNodes;
+                this.createLinks();
+            }
+        };
+        this.removeLinkByIdentifier = (identifier, attr) => {
+            const node = this.nodes.filter(node => identifier === identifier);
+            if (node.length !== 1) {
+                console.log("Node not found");
+            }
+            else {
+                node[0].removeAttribute(attr);
+            }
+        };
         this.nodes = nodes;
         this.createLinks();
     }
 }
-new Grafo(tourists);
+const grafera = new Grafo(tourists);
+console.log("LINKS: ", grafera.links);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 class GraphNode {
   identifier: string;
   attributes: {
-    [key: string]: any
+    [key: string]: any;
   };
 
-  constructor({ identifier, attributes }: {
+  constructor({
+    identifier,
+    attributes
+  }: {
     identifier: string;
     attributes?: object;
   }) {
@@ -16,22 +19,34 @@ class GraphNode {
     if (attr in this.attributes) {
       delete this.attributes[attr];
     } else {
-      console.log('Attribute not found');
+      console.log("Attribute not found");
     }
-  }
+  };
 }
 
 const tourists: GraphNode[] = [
-  new GraphNode({ identifier: 'dart', attributes: { lang: 'en' } }),
-  new GraphNode({ identifier: 'ismelo', attributes: { lang: 'en' } }),
-  new GraphNode({ identifier: 'fgod', attributes: {} }),
-  new GraphNode({ identifier: 'murtinha', attributes: {} })
+  new GraphNode({
+    identifier: "dart",
+    attributes: { lang: ["en", "fr", "pt", "es"], age: 21 }
+  }),
+  new GraphNode({
+    identifier: "ismelo",
+    attributes: { lang: ["en", "fr", "al"], age: 12 }
+  })
+  // new GraphNode({
+  //   identifier: "fgod",
+  //   attributes: { lang: ["pt", "fr", "al"] }
+  // }),
+  // new GraphNode({
+  //   identifier: "murtinha",
+  //   attributes: { lang: ["es", "fr", "al"] }
+  // })
 ];
 
 type GraphLinks = {
-  identifier: string,
-  connections: GraphNode[]
-}
+  identifier: string;
+  connections: GraphNode[];
+};
 
 class Grafo {
   nodes: GraphNode[];
@@ -46,13 +61,24 @@ class Grafo {
     this.links = [];
     this.nodes.forEach((node: GraphNode) => {
       Object.keys(node.attributes).forEach((attr: string) => {
-        const value = node.attributes[attr];
+        const values = Array.isArray(node.attributes[attr])
+          ? node.attributes[attr]
+          : [node.attributes[attr]];
+
         this.nodes.forEach((otherNode: GraphNode) => {
           if (node === otherNode) return;
-          if (attr in node.attributes && otherNode.attributes[attr] === value) {
-            this.links.push({
-              identifier: attr,
-              connections: [node, otherNode]
+          if (attr in node.attributes) {
+            const newArray = Array.isArray(otherNode.attributes[attr])
+              ? otherNode.attributes[attr]
+              : [otherNode.attributes[attr]];
+
+            values.forEach((val: any) => {
+              if (newArray.includes(val) || val in newArray) {
+                this.links.push({
+                  identifier: attr,
+                  connections: [node, otherNode]
+                });
+              }
             });
           }
         });
@@ -61,31 +87,33 @@ class Grafo {
 
     console.table(this.nodes);
     console.table(this.links);
-  }
+  };
 
   addNode = (node: GraphNode) => {
     this.nodes.push(node);
     this.createLinks();
-  }
+  };
 
   removeNodeByIdentifier = (identifier: string) => {
     const newNodes = this.nodes.filter(node => node.identifier != identifier);
     if (newNodes.length == this.nodes.length) {
-      console.log("Node not found")
+      console.log("Node not found");
     } else {
       this.nodes = newNodes;
       this.createLinks();
     }
-  }
+  };
 
   removeLinkByIdentifier = (identifier: string, attr: string) => {
     const node = this.nodes.filter(node => identifier === identifier);
     if (node.length !== 1) {
-      console.log('Node not found');
+      console.log("Node not found");
     } else {
       node[0].removeAttribute(attr);
     }
-  }
+  };
 }
 
-new Grafo(tourists);
+const grafera = new Grafo(tourists);
+
+console.log("LINKS: ", grafera.links);
