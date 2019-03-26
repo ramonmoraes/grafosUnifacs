2cadc7dab7f210baf78cd37e7bba7ece315a0234
Ramon Moraes <ramon.moraes94@gmail.com>
2019-03-26 09:10:12 -0300

    add describe to test

--- test/graphNode.test.ts
+++ test/graphNode.test.ts
@@ -1,24 +1,26 @@
-import GraphNode from '../src/graphNode';
+import GraphNode from "../src/graphNode";
 
-test('remove attribute', () => {
-  const node = new GraphNode({
-    identifier: "foo",
-    attributes: {
-      key: "val",
-    }
+describe("GraphNode", () => {
+  test("remove attribute", () => {
+    const node = new GraphNode({
+      identifier: "foo",
+      attributes: {
+        key: "val"
+      }
+    });
+
+    expect("key" in node.attributes).toBeTruthy();
+    node.removeAttribute("key");
+    expect("key" in node.attributes).toBeFalsy();
   });
 
-  expect('key' in node.attributes).toBeTruthy()
-  node.removeAttribute("key")
-  expect('key' in node.attributes).toBeFalsy()
-});
-
-test('snapshot match', () => {
-  const node = new GraphNode({
-    identifier: "foo",
-    attributes: {
-      key: "val",
-    }
+  test("snapshot match", () => {
+    const node = new GraphNode({
+      identifier: "foo",
+      attributes: {
+        key: "val"
+      }
+    });
+    expect(node).toMatchSnapshot();
   });
-  expect(node).toMatchSnapshot()
 });
