d3abe1862a749cf819ba78e1cc187bcf4e6840e3
Ramon Moraes <ramon.moraes94@gmail.com>
2019-03-26 22:23:46 -0300

    easier tests

--- test/graph.test.ts
+++ test/graph.test.ts
@@ -1,18 +1,17 @@
 import Graph, { GraphLinks } from "../src/graph";
 import GraphNode from "../src/graphNode";
 
-
 describe("Graph", () => {
   let graph: Graph;
   beforeEach(() => {
     const nodes: GraphNode[] = [
       new GraphNode({
         identifier: "dart",
-        attributes: { lang: ["en"], }
+        attributes: { lang: ["en"] }
       }),
       new GraphNode({
         identifier: "ismelo",
-        attributes: { lang: ["en"]}
+        attributes: { lang: ["en"] }
       })
     ];
     graph = new Graph(nodes);
@@ -30,16 +29,18 @@ describe("Graph", () => {
     expect(graph.nodes.length).toEqual(1);
   });
 
-  test.only("getLinksBetweenNodes", () => {
-    const links = graph.getLinkBetweenNodes(
-      graph.nodes[0],
-      graph.nodes[1]
-    );
+  test("getLinksBetweenNodes", () => {
+    const links = graph.getLinkBetweenNodes(graph.nodes[0], graph.nodes[1]);
 
     expect(links.length).toEqual(1);
-    expect(links[0].identifier).toEqual('lang');
-    expect(links[0].value).toEqual('en');
-    expect(links[0].connections[0]).toEqual(graph.nodes[0])
-    expect(links[0].connections[1]).toEqual(graph.nodes[1])
+    expect(links[0].identifier).toEqual("lang");
+    expect(links[0].value).toEqual("en");
+    expect(links[0].connections[0]).toEqual(graph.nodes[0]);
+    expect(links[0].connections[1]).toEqual(graph.nodes[1]);
+  });
+
+  test.only("createLinks", () => {
+    graph.createLinks();
+    console.log(graph.links);
   });
 });
