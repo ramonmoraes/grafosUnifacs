e84b425f600da871a6f7a634580b9b04fab690f6
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-01 23:10:09 -0300

    add isEqual utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -13,3 +13,32 @@ export function flat(arr: any[], result: any[] = []): any[] {
   }
   return result;
 }
+
+export function isEqual(arg1: any, arg2: any): boolean {
+  const keys1 = Object.keys(arg1);
+  const keys2 = Object.keys(arg2);
+
+  for (let key of keys1) {
+    if (key in keys2 || keys2.includes(key)) continue;
+    return false;
+  }
+
+  for (let key of keys1) {
+    const keyType = typeof key;
+    if (keyType === "object" && !isEqual(arg1[key], arg2[key])) {
+      console.log("at recursion");
+      return false;
+    }
+
+    if (Array.isArray(arg1[key]) && Array.isArray(arg2[key])) {
+      const array1 = arg1[key].sort();
+      const array2 = arg2[key].sort();
+      if (JSON.stringify(array1) != JSON.stringify(array2)) return false;
+    } else if (arg1[key] != arg2[key]) {
+      console.log("at value compare", arg1[key], arg2[key]);
+      return false;
+    }
+  }
+
+  return true;
+}
---
+++ test/utils.test.ts
@@ -0,0 +1,11 @@
+import { isEqual } from "../src/utils";
+
+describe.only("Utils", () => {
+  test("isEqual", () => {
+    expect(isEqual({ n: "n" }, { n: "n" })).toBeTruthy();
+    expect(isEqual({ n: "n", i: 22 }, { n: "n", i: 23 })).toBeFalsy();
+    expect(isEqual({ i: 22, n: "n" }, { n: "n", i: 22 })).toBeTruthy();
+    expect(isEqual({ a: [1, 2, 3] }, { a: [1, 2, 3] })).toBeTruthy();
+    expect(isEqual({ a: [1, 3, 2] }, { a: [1, 2, 3] })).toBeTruthy();
+  });
+});
