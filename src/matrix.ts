d78e0304ff441915052569c941c6f3babb6060de
Ramon Moraes <ramon.moraes94@gmail.com>
2019-05-21 10:00:25 -0300

    add main matrix method to matrix module

--- src/matrix.ts
+++ src/matrix.ts
@@ -21,3 +21,24 @@ export function getGraphPositionMap(graph: Graph) {
   }
   return positionMap;
 }
+
+export default function matrix (graph: Graph, filteredValue:string = "fr"): number[][] {
+  const table = graph.getSimplifiedTable(filteredValue)
+  const connections = table.map(link => link.connections)
+  const positionMap = getGraphPositionMap(graph);
+  const matrix = getEmptyMatrix(graph.nodes.length);
+  
+  connections.forEach(connection => {
+    const identifier1 = connection[0];
+    const pos1 = positionMap[identifier1];
+    const identifier2 = connection[1];
+    const pos2 = positionMap[identifier2];
+    matrix[pos1][pos2] = 1
+    matrix[pos2][pos1] = 1
+  });
+  
+  matrix.forEach((element: any) => {
+    console.log(element)
+  });
+  return matrix
+}
